2371b74e225b69cf6f8aa2fd540e8b15ee514981
Jose <jose.s.figueroa6@gmail.com>
2016-10-19 07:52:38 -0700

    Completed initial setup of project of and .gitignore configuration.

---
+++ src/main/java/edu/csupomona/cs585/CS585_Research_Project/App.java
@@ -0,0 +1,13 @@
+package edu.csupomona.cs585.CS585_Research_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
